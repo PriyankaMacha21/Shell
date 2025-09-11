c7a0341379d5c6c8e9fb524867d6c694e536bf62
PriyankaMacha21 <priyankamacha06@gmail.com>
2025-09-10 20:58:46 -0500

    Variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,5 +2,5 @@
 PERSON1 = Trump
 PERSON2 = People
 
-echo "$Person1: I like to rule the world!"
-echo "$Person2: We are happy to be ruled by you!"
+echo "$PERSON1: I like to rule the world!"
+echo "$PERSON2: We are happy to be ruled by you!"
