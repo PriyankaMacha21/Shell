6ad69df7305e870f5280557389ba6abd6a47be19
PriyankaMacha21 <priyankamacha06@gmail.com>
2025-10-15 09:32:40 -0500

    hello

---
+++ 13-colors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+R = "\e[31m"
+G = "\e[32m"
+Y = "\e[33m"
+B = "\e[34m"
+M = "\e[35m"
+C = "\e[36m"
+W = "\e[37m"
+N = "\e[0m" 
+
+USERID=$(id-u)
+if( $USERID -ne 0) 
+then
+echo -e "$R "Please run using root user"
+else
+echo -e "$G You are running using root user"
+fi
