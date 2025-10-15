b192b2dc2ddb4fcda1ccd1717d29a4c3434ff8eb
PriyankaMacha21 <priyankamacha06@gmail.com>
2025-10-15 09:42:25 -0500

    hello

--- 12-colors.sh
+++ 12-colors.sh
@@ -2,3 +2,5 @@
 echo -e "Hello World!"
 echo -e "\e[31m Hello World!"
 echo -e "Priya you are working on colors"
+echo -e "to stop the same color use \e[0m"
+echo -e "\e[35m How are you?"
--- 13-colors.sh
+++ 13-colors.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-R = "\e[31m"
-G = "\e[32m"
-Y = "\e[33m"
-B = "\e[34m"
-M = "\e[35m"
-C = "\e[36m"
-W = "\e[37m"
-N = "\e[0m" 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+W="\e[37m"
+N="\e[0m" 
 
 USERID=$(id -u)
-if( $USERID -ne 0) 
+if($USERID -ne 0) 
 then
-echo -e "$R "Please run using root user"
-else
-echo -e "$G You are running using root user"
-fi
+echo -e "$R Please run using root user $N"
+
